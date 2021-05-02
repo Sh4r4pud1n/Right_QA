0e166b7dae6627c78b555a2b342181406065c5d5
Sh4r4pud1n <47586453+Sh4r4pud1n@users.noreply.github.com>
2021-05-02 21:12:56 +0300

    Creted tasks

--- Right_QA/Lection1/src/FirstTask.java
+++ Right_QA/Lection1/src/FirstTask.java
@@ -1,2 +1,20 @@
-public class FirstTask {
+
+
+    /**
+    Write a simple Java program using any text editor.
+    Compile this program using Java compiler and run it from the console.
+    You can write whatever you want, but program should compile and should print something to a console.
+
+     */
+    package com.company;
+    public class FirstTask {
+           public static void main (String[] args) {
+            String a = "Hello World";
+            System.out.print(a);
+
+        }
+
+
 }
+
+
---
+++ Right_QA/Lection1/src/SecondTask.java
@@ -0,0 +1,29 @@
+import java.lang.String;
+public class SecondTask {
+    /**
+     Write a program which creates and initializes all primitive data types. Remark: There are 8 primitive data types.
+     */
+
+
+        public static void main (String[] args) {
+            byte a = 123;
+            System.out.println(a);
+            short b = 4;
+            System.out.println(b);
+            int c = 567;
+            System.out.println(c);
+            long x = 1345243;
+            System.out.println(x);
+            float z = 1.23f;
+            System.out.println(z);
+            double y = 1.342536474d;
+            System.out.println(y);
+            boolean AllRight = true;
+            System.out.println(AllRight);
+            char k = 'K';
+            System.out.println(k);
+            String title = "Hello World!";
+            System.out.println(title);
+
+    }
+}
